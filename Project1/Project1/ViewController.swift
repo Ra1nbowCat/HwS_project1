2f28da2b64fd4409e0cea42fd2a72f1160b3bc40
Lekhov Ilya <solwanlomtohs@mail.ru>
2022-06-25 11:08:50 +0300

    Added codable protocol to numberOfShows

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -10,21 +10,40 @@ import UIKit
 class ViewController: UITableViewController {
     
     var pictures = [String]()
+    var numberOfShows = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let fm = FileManager.default
-        let path = Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path)
         
         title = "Storm Viewer"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let defaults = UserDefaults.standard
+        if let savedNumber = defaults.object(forKey: "numberOfShows") as? Data {
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                numberOfShows = try jsonDecoder.decode(Int.self, from: savedNumber)
+            } catch {
+                print("Failed to decode number")
+            }
+        }
+        
+        performSelector(inBackground: #selector(loadingList), with: nil)
+    }
+    
+    @objc func loadingList() {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
         
         for item in items {
             if item.hasPrefix("nssl"){
                 pictures.append(item)
             }
         }
+        
+        tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -34,7 +53,6 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         pictures.sort()
-        //cell.textLabel?.text = pictures[indexPath.row]
         cell.textLabel?.text = "Picture \(pictures.firstIndex(of: pictures[indexPath.row])! + 1) of \(pictures.count)"
         return cell
     }
@@ -43,6 +61,20 @@ class ViewController: UITableViewController {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
             vc.selectedImage = pictures[indexPath.row]
             navigationController?.pushViewController(vc, animated: true)
+            numberOfShows += 1
+            save()
+            print(numberOfShows)
+        }
+    }
+    
+    func save() {
+        let jsonEncoder = JSONEncoder()
+        
+        if let savedData = try? jsonEncoder.encode(numberOfShows) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "numberOfShows")
+        } else {
+            print("Failed to encode")
         }
     }
 }
